59f34de53cabb581d1416766ecd33528b96d4427
Alex <alex@example.com>
2020-10-03 18:05:38 +0400

    feat: add fuctions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let minValue = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < minValue) {
+        minValue = array[i];
+      }
+    }
+    return minValue;
+  }
+    
 }
 
 exports.max = function max (array) {
-  return 0;
+  
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let maxValue = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > maxValue) {
+        maxValue = array[i];
+      }
+    }
+    return maxValue;
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let value = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      value += array[i];
+    }
+  
+    let result = value / array.length;
+    return result; 
+  }
+    
 }
